c2b9184cda916350f4e9614490fd44edc306ce52
ckaznable <kami25565@gmail.com>
2023-08-09 14:40:32 +0800

    perf: imporve last staus cache

--- main.py
+++ main.py
@@ -10,40 +10,44 @@ class LastStatus:
         if not os.path.isfile("last_status"):
             with open("last_status", "w") as f:
                 json.dump(self.data, f)
+        else:
+            with open("last_status", "r") as f:
+                self.data = json.load(f)
 
-        with open("last_status", "r") as f:
-            self.data = json.load(f)
+    def get_boards_list(self):
+        list = []
+        for board in self.data:
+            list.append(board[0])
+        return list
 
     def save(self):
         with open("last_status", "w") as f:
             json.dump(self.data, f)
 
-    def set(self, id, value):
+    def set(self, id, value, condition):
+        data = [id, value, condition]
         obj = self.find(id)
         if obj != None:
-            index, _ = obj
-            self.data[index][1] = value
+            _, index, _ = obj
+            self.data[index] = data
         else:
-            self.data.append([id, value])
+            self.data.append(data)
 
     def get(self, id):
         obj = self.find(id)
         if obj != None:
-            _, data = obj
-            return data
+            _, index, condition = obj
+            return (index, condition)
         return None
 
     def find(self, id):
         for (i, x) in enumerate(self.data):
             if x[0] == id:
-                return (i, x[1])
+                return (i, x[1], x[2])
         return None
 
-def get_board_list(env):
-    return env.split(',')
-
-def get_post(ptt, board, limit=10, index=0):
-    newest_index = ptt.get_newest_index(PyPtt.NewIndex.BOARD, board)
+def get_post(ptt, board, limit=10, index=0, condition=None):
+    newest_index = ptt.get_newest_index(PyPtt.NewIndex.BOARD, board, search_condition=condition, search_type=PyPtt.SearchType.KEYWORD)
     if index >= newest_index:
         return []
 
@@ -55,7 +59,11 @@ def get_post(ptt, board, limit=10, index=0):
     while newest_count < newest_range:
         cur_index = newest_index - newest_count
         newest_count += 1
-        post = ptt.get_post(board, index=cur_index)
+
+        if index >= cur_index:
+            continue
+
+        post = ptt.get_post(board, index=cur_index, search_condition=condition, search_type=PyPtt.SearchType.KEYWORD)
 
         if not post:
             continue
@@ -74,36 +82,40 @@ def get_post(ptt, board, limit=10, index=0):
     return posts
 
 def extract_post(post):
-    return (post["title"], post["index"])
+    return (post["aid"], post["title"], post["index"])
 
 def extract_posts(posts):
     return list(map(extract_post, posts))
 
+def process(posts):
+    print(posts)
+
 if __name__ == "__main__":
     config = dotenv_values(".env")
-    boards = get_board_list(config.get("boards"))
-    ptt = PyPtt.API()
     status = LastStatus()
+    boards = status.get_boards_list()
+    ptt = PyPtt.API()
     posts = []
 
     try:
         ptt.login(config.get("username"), config.get("pw"))
 
         for board in boards:
-            # try:
             print("Getting posts from " + board)
 
-            index = status.get(board)
-            if index == None:
-                index = 0
+            index = 0
+            condition = None
+            s = status.get(board)
+            if s != None:
+                index = s[0]
+                condition = s[1]
 
-            posts = extract_posts(get_post(ptt, board, index=index))
+            posts = extract_posts(get_post(ptt, board, index=index, condition=condition))
 
             if len(posts) > 0:
-                status.set(board, posts[0][1])
+                status.set(board, posts[0][2], condition)
 
-        print(status.data)
-        print(posts)
+        process(posts)
 
     finally:
         ptt.logout()
